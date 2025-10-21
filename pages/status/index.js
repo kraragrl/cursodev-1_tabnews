66b93d50fd9a39f8580d8b4ef4656d42ed54cf53
Clara Fernandes Pereira <41123151+kraragrl@users.noreply.github.com>
2025-10-21 21:34:47 +0000

    feat: add initial version of `/status` page

---
+++ pages/status/index.js
@@ -0,0 +1,56 @@
+import useSWR from "swr";
+
+async function fetchAPI(key) {
+  const response = await fetch(key);
+  const responseBody = await response.json();
+  return responseBody;
+}
+
+export default function StatusPage() {
+  return (
+    <>
+      <h1>Status</h1>
+      <DatabaseStatus />
+      <UpdatedAt />
+    </>
+  );
+}
+
+function UpdatedAt() {
+  const { isLoading, data } = useSWR("/api/v1/status", fetchAPI, {
+    refreshInterval: 2000,
+  });
+
+  let updatedAtText = "Carregando...";
+
+  if (!isLoading && data) {
+    updatedAtText = new Date(data.updated_at).toLocaleString("pt-BR");
+  }
+
+  return <div>Última atualização: {updatedAtText}</div>;
+}
+
+function DatabaseStatus() {
+  const { isLoading, data } = useSWR("/api/v1/status", fetchAPI, {
+    refreshInterval: 2000,
+  });
+
+  let databaseText = "Carregando...";
+
+  if (!isLoading && data) {
+    databaseText = data.dependencies.database;
+  }
+
+  return (
+    <>
+      <div>
+        <h3>Informações do banco de dados:</h3>
+        <ul>
+          <li> Versão: {databaseText.version}</li>
+          <li> Conexões Abertas: {databaseText.opened_connections}</li>
+          <li> Conexões Máximas: {databaseText.max_connections} </li>
+        </ul>
+      </div>
+    </>
+  );
+}
